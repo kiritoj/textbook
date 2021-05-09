5bd8fc4dc7be09c2970afcd3b6af9e6eb0f2e134
kirito <2552744904@qq.com>
2021-05-10 01:09:36 +0800

    启动页

---
+++ app/src/main/java/com/leo/copytoutiao/activity/BootActivity.java
@@ -0,0 +1,31 @@
+package com.leo.copytoutiao.activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
+import com.leo.copytoutiao.R;
+
+import org.jetbrains.annotations.NotNull;
+
+public class BootActivity extends AppCompatActivity {
+
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(@NonNull @NotNull Message msg) {
+            super.handleMessage(msg);
+            LoginActivity.startActivity(BootActivity.this,"");
+            finish();
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boot);
+        handler.sendEmptyMessageDelayed(1,2000);
+    }
+}
